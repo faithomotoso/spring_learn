97a555252e68d26303b6332155a224a8fe46336f
Faith Omotoso <faithomotoso@gmail.com>
2023-08-21 21:18:11 +0100

    Swtiched to constructor injection with qualifier annotation Load and inject values from .properties file

--- src/main/java/com/exmp/springdemoannotations/coach/TennisCoach.java
+++ src/main/java/com/exmp/springdemoannotations/coach/TennisCoach.java
@@ -3,31 +3,34 @@ package com.exmp.springdemoannotations.coach;
 import com.exmp.springdemoannotations.fortune.FortuneService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TennisCoach implements Coach {
     private FortuneService fortuneService;
 
-//    @Autowired
-//    public TennisCoach(FortuneService fortuneService) {
-//        this.fortuneService = fortuneService;
-//    }
+    @Value("${tennis.team}")
+    private String teamName;
+    @Autowired
+    public TennisCoach(@Qualifier("randomFortuneService") FortuneService fortuneService) {
+        this.fortuneService = fortuneService;
+    }
 
 //    @Autowired
 //    public void setFortuneService(FortuneService fortuneService) {
 //        this.fortuneService = fortuneService;
 //    }
 
-    @Autowired
-    @Qualifier("randomFortuneService")
-    public void doFortuneThings(FortuneService fortuneService) {
-        System.out.println("Method injected fortune service says: " + fortuneService.getFortune());
-        this.fortuneService = fortuneService;
-    }
+    //    @Autowired
+//    @Qualifier("randomFortuneService")
+//    public void doFortuneThings(FortuneService fortuneService) {
+//        System.out.println("Method injected fortune service says: " + fortuneService.getFortune());
+//        this.fortuneService = fortuneService;
+//    }
     @Override
     public String getDailyWorkout() {
-        return "Practice backhand volley";
+        return "When playing for " + this.teamName + ". " + "Practice backhand volley";
     }
 
     @Override
